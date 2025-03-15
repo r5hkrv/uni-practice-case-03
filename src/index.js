aadf2687be15c7d215ab7f7174dd86f1e7d07616
r5hkrv <202286897+r5hkrv@users.noreply.github.com>
2025-03-16 00:12:51 +0400

    refactor: separate demo and slider styles; use dataset selectors

--- src/index.js
+++ src/index.js
@@ -1,67 +1,23 @@
-const optsParseError = (error = "Failed to parse options") => ({
-	opts: null,
-	isError: true,
-	error,
-});
-
-const parseOpts = (o) => {
-	const result = {};
-
-	if ("root" in o) {
-		result.root = o.root;
-	} else return optsParseError("No root element selector");
-
-	if ("slide" in o) {
-		result.slide = o.slide;
-	} else return optsParseError("No slide element selector");
-
-	if ("arrows" in o && "left" in o.arrows && "right" in o.arrows) {
-		result.arrows = o.arrows;
-	} else if ("left" in o.arrows) {
-		return optsParseError("No right arrow element selector");
-	} else if ("right" in o.arrows) {
-		return optsParseError("No left arrow element selector");
-	} else return optsParseError("No arrow element selectors");
-
-	if ("counts" in o && "current" in o.counts && "total" in o.counts) {
-		result.counts = o.counts;
-	} else if ("current" in o) {
-		return optsParseError("No total count element selector");
-	} else if ("total" in o) {
-		return optsParseError("No current count element selector");
-	} else return optsParseError("No count element selectors");
-
-	return { opts: result, isError: false, error: "" };
+const querySliderElem = (key, root = document) => {
+	return root.querySelector(`[data-slider="${key}"]`);
 };
 
-const Slider = (o) => {
-	const { opts, isError, error } = parseOpts(o);
-
-	if (isError) {
-		console.error(error);
-
-		return;
-	}
-
-	const rootElem = document.querySelector(opts.root);
-	const imgElems = rootElem.querySelectorAll(opts.slide);
-	const leftArrowElem = rootElem.querySelector(opts.arrows.left);
-	const rightArrowElem = rootElem.querySelector(opts.arrows.right);
-	const currCountElem = rootElem.querySelector(opts.counts.current);
-	const totalCountElem = rootElem.querySelector(opts.counts.total);
+const Slider = (rootElem = querySliderElem("root")) => {
+	const prevArrowElem = querySliderElem("arrow-prev", rootElem);
+	const nextArrowElem = querySliderElem("arrow-next", rootElem);
+	const slidesElem = querySliderElem("slides", rootElem);
 
 	if (rootElem === null) return;
-	if (imgElems.length === 0) return;
-	if (leftArrowElem === null || rightArrowElem === null) return;
-	if (currCountElem === null || totalCountElem === null) return;
+	if (!rootElem.hasAttribute("[data-slider='root']")) return;
+	if (prevArrowElem === null || nextArrowElem === null) return;
+	if (slidesElem === null || slidesElem.children.length === 0) return;
+
+	const imgElems = slidesElem.children;
 
 	let imgIndex = 0;
 
-	currCountElem.textContent = imgIndex + 1;
-	totalCountElem.textContent = imgElems.length;
-
-	leftArrowElem.addEventListener("click", () => {
-		imgElems[imgIndex].classList.remove("is-selected");
+	prevArrowElem.addEventListener("click", () => {
+		imgElems[imgIndex].removeAttribute("data-slider");
 
 		imgIndex -= 1;
 
@@ -69,13 +25,11 @@ const Slider = (o) => {
 			imgIndex = imgElems.length - 1;
 		}
 
-		currCountElem.textContent = imgIndex + 1;
-
-		imgElems[imgIndex].classList.add("is-selected");
+		imgElems[imgIndex].setAttribute("data-slider", "selection");
 	});
 
-	rightArrowElem.addEventListener("click", () => {
-		imgElems[imgIndex].classList.remove("is-selected");
+	nextArrowElem.addEventListener("click", () => {
+		imgElems[imgIndex].removeAttribute("data-slider");
 
 		imgIndex += 1;
 
@@ -83,9 +37,7 @@ const Slider = (o) => {
 			imgIndex = 0;
 		}
 
-		currCountElem.textContent = imgIndex + 1;
-
-		imgElems[imgIndex].classList.add("is-selected");
+		imgElems[imgIndex].setAttribute("data-slider", "selection");
 	});
 };
 
